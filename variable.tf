bd975c74c349b54fd295c703c689189c43a844a0
Pawel <92589562+zukeras@users.noreply.github.com>
2023-09-29 16:20:33 +0200

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+    type = string
+    description = "grup name"
+    default = "app_grp"
+}
+variable "location" {
+    type = string
+    description = "location"
+    default = "North Europe"
+}
